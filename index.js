fe286b1b2ce9b7e51d12cf281cbb65735c2ddafd
ziitania <tjhaweszii@gmail.com>
2021-03-15 11:42:00 -0700

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(height){
   // Write your code here!
+  if(height <= 400){
+    return 'This one is on me!';
+  } else if (height > 2500) {
+    return 'No can do.';
+  } else if (height > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  return;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if(city == 'NYC'){
+    return 'Ok, sounds good.';
+  } else {
+    return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if(tip == 'generous') {
+    return 'Thank you so much.';
+  } else if (tip == 'not as generous'){
+    return 'Thank you.';
+  } else {
+    return 'Bye.'
+  }
 }
